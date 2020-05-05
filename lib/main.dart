eac0a1b85765eda36d7d7328ffb7a32c13b83c6b
fakhrirahman <fakhrirahman11@gmail.com>
2020-05-06 02:20:31 +0700

    layout column dan row1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                        'Aci Ganteng Banget',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                      'Bawahnya',
+                    style: TextStyle(color: Colors.grey[500]),
+                  )
+                ],
+              )),
+          Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(Color, Icons.call, 'CALL'),
+          _buildButtonColumn(Color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(Color, Icons.share, 'SHARE'),
+        ],
+      ),
+    )
+    Column _buildButtonColumn(Color color, IconData icon, String label){
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          )
+        ],
+      )
+
+    }
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Flutter Layout Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Flutter Layout Demo'),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: <Widget>[
+            titleSection,
+            buttonSection,
+          ],
         ),
       ),
     );
